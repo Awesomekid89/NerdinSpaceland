cd664c3d81522623234aef5de22b6e2d89016a77
Awesomekid89 <73956641+Awesomekid89@users.noreply.github.com>
2021-05-14 11:11:55 -0700

    Create NerdinSpaceland.py

---
+++ NerdinSpaceland.py
@@ -0,0 +1,63 @@
+print('#########################')
+print('A Nerds Adventure')
+print('#########################')
+
+print('Chapter 1')
+
+# Going into Chapter 1
+
+
+print('I cracked his shield, he is low.')
+print('Yeah I see him, good shot!')
+print('Thanks.')
+print('Time for dinner Kyle!')
+print('I got to go guys.')
+print('See ya!')
+# Kyle walks to the dinner table and starts eating.
+# Kyle's mother tells Kyle they are going to a themepark after dinner.
+# Kyle gets done eating and goes upstairs to get ready.
+# Kyle's family gets into the car.
+print('Are you excited Kyle?')
+print('Yeah! I have never been to this themepark.')
+print('They have the setting set to the 1980s is what I have heard so far.')
+# Kyle's family drives up to the themepark and goes to the ticket booth.
+print('Can we get 4 tickets?')
+print('Yes, that will be $50.')
+# Kyle's mother gives the person at the ticket booth $50 US dollars.
+# Kyle and his siblings go into the park.
+print('Man, this is such a nice and wonderful themed park.')
+# Kyle goes and does his rides at the themepark and decides to get ready to get off the ride.
+print('Man, that was a good ride!')
+# Kyle notices that he does not see his siblings nearby.
+# Kyle goes and looks for his siblings, only to not find them in the whole themepark.
+print('Hey, where is everybody at? I thought the themepark did not close until 6:30 PM.')
+print('I better check the front entrance to see if my parents are waiting for me.')
+# Kyle goes to the front entrance and see that the lock is twisted the right, symboling that the park is closed.
+print('Why is the front entrance door locked?')
+# Kyle goes behind the ticket booth and sees that there are no cars in the parking lot, not even his family's car.
+print('What?! Where did my family go?!')
+# Kyle sits down for a minute and thinks what just happened. 
+print('This cannot be happening right now!')
+# Kyle then looks around and see's a poster board that was not at the front entrance before.
+# Kyle walks over to the poster board.
+print('Was this poster board here? Wait, why does this say "Zombies in Spaceland Survial Park"')
+# Kyle was breaking down the best scenario in his mind.
+# Kyle then hears a low moan, Kyle thinks the low moan sounds like a zombie.
+print('You have got to be kidding me! First I get abandonded, then I have to survive this themepark?!')
+# Kyle is furious and see's a M1 Garand in the corner of the ticket booth. 
+# Kyle thinks about grabbing the gun and trying to kill the zombie.
+print('I doubt I can get a good shot, since I have never shot a gun before.')
+# Kyle picks up the gun and walks outside the ticket booth.
+print('The gig is over! Just show yourself!')
+# Kyle thinks that the zombie is a person dressed up in a costume.
+# Kyle puts the gun butt up under his arm pit and aims down the sights.
+# Bam! Kyle shoots the M1 Garand. 
+# Kyle then realizes that the zombie is not going down onto the floor.
+print('Come on! That was a clear shot!')
+# Kyle raises the gun up again and notices that there are no sockets in the zombies eyes.
+# Kyle quickly realizes that the poster board was real and the zombie is real. 
+# Kyle runs to a safe spot to think.
+print('The best thing to do is supply power to the themepark, since the power mysteriously is down.')
+# Kyle realizes that the zombie problem will only get worse.
+print('Second objective, find components to find to make a make-shift sleeping bag or bed.')
+# Kyle goes to the poster board and reads the instructions. 
